0b711205fc29fd6e84cfde42285be636343679f5
lizx <lizx@nbport.com.cn>
2022-09-20 19:02:48 +0800

    初始化

---
+++ src/main/java/com/guapixu/limit/LimitApplication.java
@@ -0,0 +1,15 @@
+package com.guapixu.limit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lizx
+ * @since 2022-09-20
+ */
+@SpringBootApplication
+public class LimitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LimitApplication.class,args);
+    }
+}
